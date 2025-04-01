b635cd22c9ddb1119b65a70d1a130cd1100581fb
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-04-01 19:15:14 -0300

    feat: finished pessoal screen

--- pages/Pessoal.js
+++ pages/Pessoal.js
@@ -1,18 +1,40 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Linking } from "react-native";
 import { Image } from 'expo-image';
 
 
 export default function PessoalScreen() {
 
+    const openInstagram = () => {
+        Linking.openURL('https://www.instagram.com/imlaurinhaaa/?next=%2Fimlaurinhaaa%2F');
+    };
+
+    const openLinkedIn = () => {
+        Linking.openURL('https://www.linkedin.com/in/laura-ferreira-violla-a526b12b1/');
+    };
+
+    const openGitHub = () => {
+        Linking.openURL('https://github.com/imlaurinhaaa');
+    };
+
     return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Laura Ferreira Violla</Text>
-            <Image source={require('../assets/IMG_20231002_155028.jpg')} />
+            <Image style={styles.img} source={require('../img/laura.jpg')} />
             <Text style={styles.emojis}>🌷✨🤍</Text>
             <Text style={styles.texto}>Sou aluna da instituição SENAI e realizo o curso Desenvolvimento de Sistemas</Text>
             <Text style={styles.texto}>Estou no 3° ano do curso e tenho 17 anos</Text>
-            <Text style={styles.social}>LinkedIn: Laura Ferreira Violla</Text>
-            <Text style={styles.social}>GitHub: imlaurinhaaa</Text>
+            <Text style={styles.subtitle}>Redes sociais</Text>
+            <View style={{ flexDirection: "row", justifyContent: "center", gap: 20 }}>
+                <TouchableOpacity onPress={openInstagram} >
+                    <Image style={styles.social} source={require('../img/insta.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={openLinkedIn} >
+                <Image style={styles.social} source={require('../img/linkedin.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={openGitHub} >
+                <Image style={styles.social} source={require('../img/github.png')} />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -20,15 +42,26 @@ export default function PessoalScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingVertical: 100,
-        paddingHorizontal: 25,
+        paddingVertical: 10,
         gap: 20,
-        backgroundColor: "#EDEEF1",
+        backgroundColor: "#F6EEE0",
     },
     titulo: {
         fontSize: 32,
         textAlign: "center",
-        color: "#FF0080",
+        color: "#821D30",
+        fontWeight: "bold"
+    },
+    img: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        alignSelf: "center",
+    },
+    subtitle: {
+        fontSize: 20,
+        textAlign: "center",
+        color: "#821D30",
         fontWeight: "bold"
     },
     emojis: {
@@ -40,8 +73,10 @@ const styles = StyleSheet.create({
         textAlign: "center",
     },
     social: {
-        fontSize: 15,
-        color: "#FB6B90",
-        fontWeight: "bold"
-    }
+        width: 80,
+        height: 80,
+        borderRadius: 10,
+        alignSelf: "center",
+        marginTop: 10,
+    },
 });
